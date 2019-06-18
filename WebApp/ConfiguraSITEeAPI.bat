7e4b77d6bb1305d63e7b670869a4530b97ae2c66
Allan Schüssler <allan_longhi@hotmail.com>
2019-06-17 22:01:45 -0300

    Alteração README e script de inicialização dos servidores

--- WebApp/ConfiguraSITEeAPI.bat
+++ WebApp/ConfiguraSITEeAPI.bat
@@ -10,6 +10,8 @@ cd C:\Windows\System32\inetsrv
 
 appcmd add apppool /name:CityWeather /managedRuntimeVersion:v4.0 /managedPipelineMode:Integrated
 
+appcmd set AppPool CityWeather -processModel.identityType:LocalSystem
+
 appcmd add site /name:CityWeather /id:100 /physicalPath:%mypath%\api\Publish /bindings:http/*:49985:
 
 appcmd set site /site.name:CityWeather /[path='/'].applicationPool:CityWeather
